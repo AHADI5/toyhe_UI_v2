1885d0fff4e0c677b5dd2c799f63e0bdf93c6644
AHADI5 <gloireahadi9@gmail.com>
2025-03-21 17:45:25 +0200

    making / the default page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { BrowserRouter as  Routes,Route } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import SignInForm from "./toyhe_app/auth/signinform";
 import AppLayout from "./toyhe_lib/component/navigation/app_layout";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/signin" element={<SignInForm />} />
+      <Route path="/" element={<SignInForm />} />
       <Route path="/app" element={<AppLayout />}>
         <Route index element={<h1>Dashboard</h1>} />
-        <Route path="profile" element={<h1>Profile</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
       </Route>
     </Routes>
   );
