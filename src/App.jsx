4cbad3795fc0f6c58fdc6bc78f9898a452c09fd3
AHADI5 <gloireahadi9@gmail.com>
2025-03-21 17:26:54 +0200

    resolving app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import React from "react";
 import SignInForm from "./toyhe_app/auth/SignInForm";
-import AppLayout from "./toyhe_lib/component/navigation/AppLayout";
+import AppLayout from "./toyhe_lib/component/navigation/app_layout";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="signin" element={<SignInForm />} />
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<h1>Dashboard</h1>} />
-          <Route path="profile" element={<h1>Profile</h1>} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="signin" element={<SignInForm />} />
+      <Route path="app" element={<AppLayout />}>
+        <Route index element={<h1>Dashboard</h1>} />
+        <Route path="profile" element={<h1>Profile</h1>} />
+      </Route>
+    </Routes>
   );
 };
 
