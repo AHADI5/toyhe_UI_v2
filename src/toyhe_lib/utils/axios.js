3956fc40de116a16459e5fc296927e6f60697663
AHADI5 <gloireahadi9@gmail.com>
2025-03-21 18:03:56 +0200

    updating the baseurl

--- src/toyhe_lib/utils/axios.js
+++ src/toyhe_lib/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://toyhe-2.onrender.com/",
+    baseURL: "https://toyhe-2.onrender.com/api/v1",
     //   baseURL: "http://192.168.43.178:8080",
     //baseURL :  "http://localhost:8080/api/v1"
 });
